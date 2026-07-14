cc666eb0634801f99674c476fcc2a0ce4fa83d1c
Olga Brun <brunolga2026@gmail.com>
2026-07-14 11:21:30 +0300

    webinar2

--- Lesson1/hw1.py
+++ Lesson1/hw1.py
@@ -13,7 +13,7 @@ print_string_reverse(" ")
 #************************************
 
 def is_isr_phone_number(phone):
-    if phone is None or phone.strip() ==:
+    if phone is None or phone.strip() == "":
         return None
     if len(phone)!=10:
         return False
@@ -24,6 +24,40 @@ def is_isr_phone_number(phone):
     return True
 
 print(is_isr_phone_number("0521234567"))
-print()
 print(is_isr_phone_number("52358879"))
-pr
+print(is_isr_phone_number("5235887a9"))
+print(is_isr_phone_number(""))
+
+def print_substring_reverse(s, start, finish):
+    if s is None or s.strip() == "":
+        print("Wrong args")
+        return
+    if start < 0 or finish >= len(s) or start > finish:
+        print("Wrong args")
+        return
+    beginning = s[:start]
+    middle_reversed = s[start:finish+1][::-1]
+    end = s[finish+1:]
+    print(beginning+middle_reversed+end)
+
+print_substring_reverse("Shalom",1,3)
+print_substring_reverse(None,1,3)
+print_substring_reverse("Hi",1,5)
+print_substring_reverse("Hi",3,1)
+
+
+def get_words_reverse(s):
+    words = s.split()
+    reversed_words = words[::-1]
+    return " ".join(reversed_words)
+
+print(get_words_reverse("Hello my nice world"))
+
+def print_words_reverse_in_columns(s):
+    words = s.split()
+    for word in words:
+        print(word[::-1])
+
+print()
+print_words_reverse_in_columns("Hello my nice world")
+
